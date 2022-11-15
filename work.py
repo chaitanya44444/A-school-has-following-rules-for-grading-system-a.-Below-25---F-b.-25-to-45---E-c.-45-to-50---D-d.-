446aa5d8e5414644903fe02975e1b1c4dbe92f55
chaitanya44444 <93338022+chaitanya44444@users.noreply.github.com>
2022-11-15 16:05:39 +0530

    Create work.py

---
+++ work.py
@@ -0,0 +1,12 @@
+result=int(input("what marks do you have?"))
+if result>80:
+    print("A grade achived ")
+elif 80>result and result>60:
+    print("B grade achived")
+elif 60>result and result<50:
+    print("C grade achieved")
+elif 50>result and result<45:
+    print("D grade achieved")
+elif 45>result and result<25:
+    print("E grade achieved")
+
